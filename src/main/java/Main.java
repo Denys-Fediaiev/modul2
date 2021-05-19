7bd1c342d21f4cbc0bb6a93f6fb8276b1302a815
raydavis <wingeeddd2gmail.com>
2021-05-19 19:23:47 +0300

    Best commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,10 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -14,14 +17,15 @@ public class Main {
         long maxSize = 99;
         String regEx = "t.*";
         String keyLine = "Our World is so big!";
-        System.out.println(findFile(directoryToSearch, searchFile, minSize, maxSize, regEx,keyLine));
+        System.out.println(findFile(directoryToSearch, searchFile, minSize, maxSize, regEx, keyLine));
+        System.out.println(searchFileByKeyWord("/Users/raydavis/Desktop/developmentJAVA/Modul2/freedom.txt","Our"));
 
     }
 
-    public static List<Path> findFile(String directory, String name, long minSize, long maxSize, String regEx,String keyLine) throws IOException {
+    public static List<Path> findFile(String directory, String name, long minSize, long maxSize, String regEx, String keyLine) throws IOException {
         try (Stream<Path> files = Files.walk(Paths.get(directory))) {
             return files
-                    .filter(p -> p.getFileName().toString().equals(name) || p.toFile().getName().matches(regEx) || p.toFile().toString().equals(keyLine))
+                    .filter(p -> p.getFileName().toString().equals(name) || p.toFile().getName().matches(regEx))
                     .filter(p -> {
                         try {
                             return Files.size(p) < maxSize && Files.size(p) > minSize;
@@ -35,28 +39,29 @@ public class Main {
         }
     }
 
-    public static void findByKeyLine(String keyLine) {
+    public static List<String> searchFileByKeyWord(String path, String match) {
+
+        List<String> linesToPresent = new ArrayList<>();
+
+        File f = new File("/Users/raydavis/Desktop/developmentJAVA/Modul2/freedom.txt");
+        FileReader fr;
         try {
-            File file = new File("/Users/raydavis/Desktop/developmentJAVA/Modul2/freedom.txt");
-            FileReader fr = new FileReader(file);
-            BufferedReader reader = new BufferedReader(fr);
-            // считаем сначала первую строку
-            String line = reader.readLine();
-            while (line != null) {
-                System.out.println(line);
-                // считываем остальные строки в цикле
-                line = reader.readLine();
-                if (line == keyLine) {
-                    System.out.println("here it is");
-                } else System.out.println("nothing here");
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            fr = new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
+            String line;
+            do {
+                line = br.readLine();
+                Pattern p = Pattern.compile(match);
+                Matcher m = p.matcher("Our");
+                if (m.find())
+                    linesToPresent.add(line);
+            } while (line != null);
+
+            br.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-
-        return ;
+        return linesToPresent;
     }
-    }
+}
