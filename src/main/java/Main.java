b994040b02075deca2e9b3e12a485c382f328563
raydavis <wingeeddd2gmail.com>
2021-05-20 14:16:07 +0300

    Commit 99

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,11 +17,10 @@ public class Main {
         long maxSize = 99;
         String regEx = "t.*";
         String searchString = "World";
-        String keyLine = "Our World is so big!";
-        System.out.println(findFile(directoryToSearch, searchFile, minSize, maxSize, regEx, searchString));
+        System.out.println(findFile(directoryToSearch, searchFile, minSize, maxSize, regEx, searchString, path));
     }
-    public static List<Path> findFile(String directory, String name, long minSize, long maxSize, String regEx, String searchString) throws IOException {
-        searchFileByKeyWord(searchString);
+    public static List<Path> findFile(String directory, String name, long minSize, long maxSize, String regEx, String searchString, String path) throws IOException {
+        searchFileByKeyWord(searchString,path);
         try (Stream<Path> files = Files.walk(Paths.get(directory))) {
             return files
                     .filter(p -> p.getFileName().toString().equals(name) || p.toFile().getName().matches(regEx))
@@ -37,9 +36,9 @@ public class Main {
                     .collect(Collectors.toList());
         }
     }
-    public static void searchFileByKeyWord(String searchString) {
+    public static boolean searchFileByKeyWord(String searchString, String path) {
         List<String> linesToPresent = new ArrayList<>();
-        File f = new File("/Users/raydavis/Desktop/developmentJAVA/Modul2/freedom.txt");
+        File f = new File(path);
         FileReader fr;
         try {
             fr = new FileReader(f);
@@ -47,15 +46,17 @@ public class Main {
             String line;
             do {
                 line = br.readLine();
-                Pattern p = Pattern.compile("World");
+                Pattern p = Pattern.compile(searchString);
                 Matcher m = p.matcher("World");
                 if (m.find())
                     linesToPresent.add(line);
             } while (line != null);
             br.close();
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
         System.out.println(linesToPresent);
+        return true;
     }
 }
